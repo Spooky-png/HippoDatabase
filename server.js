8baeb4d491182b4ea5d75e191c38052340dd9bf5
Spooky-png <MikeHinds4@gmail.com>
2020-01-13 14:43:40 -0800

    Hippo Database. Remember to check commands in future.

--- server.js
+++ server.js
@@ -9,28 +9,27 @@ const session = require('express-session');
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:/quotes_db', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:/hippos_db', { useNewUrlParser: true });
 
-const UserSchema = new mongoose.Schema({
+const HippoSchema = new mongoose.Schema({
     name: {type: String, required: true, minlength: 2},
-    quote: {type: String, required: true, minlength: 6},
+    description: {type: String, required: true, minlength: 6},
 }, {timestamps: true })
-const User = mongoose.model('User', UserSchema);
+const Hippo = mongoose.model('Hippo', HippoSchema);
 
 const flash = require('express-flash');
 app.use(flash());
-app.post('/users', (req, res) => {
-    const user = new User();
-    user.name = req.body.name;
-    user.quote = req.body.quote;
-    user.save()
+app.post('/create', (req, res) => {
+    const hippo = new Hippo();
+    hippo.name = req.body.name;
+    hippo.description = req.body.description;
+    hippo.save()
         .then(newUserData => {
-            console.log('User created: ', newUserData)
+            console.log('Hippo logged: ', newHippoData)
             res.redirect('/quotes');
         })
         .catch(err => {
             console.log("We have an error!", err);
-            // adjust the code below as needed to create a flash message with the tag and content you would like
             for (var key in err.errors) {
                 req.flash('registration', err.errors[key].message);
             }
@@ -45,26 +44,43 @@ app.use(session({
     cookie: { maxAge: 60000 }
   }))
 
-app.post('/users', (req, res) => {
-    const user = new User();
-    user.name = req.body.name;
-    user.quote = req.body.quote;
-    user.save()
-        .then(newUserData => {
-            console.log('User created: ', newUserData)
-            res.redirect('/quotes');
-        })
-        .catch(err => console.log(err));
-})
-
-app.get('/', (req, res) => {
-    res.render("index")
+app.get('/hippos/new', (req, res) => {
+    res.render('index')
 });
-app.get('/quotes', (req, res) => {
-    User.find()
-        .then(data => res.render("quotes", { Users: data.reverse() }))
+app.get('/', (req, res) => {
+    Hippo.find()
+        .then(data => res.render("hippos", { Hippos: data }))
         .catch(err => res.json(err));
 });
+app.get('/edit/:id', (req, res) => {
+    var hippo_id = req.params.id;
+    Hippo.findOne({_id: hippo_id})
+    .then(data => res.render("single", { Hippo: data }))
+        .catch(err => res.json(err));
+})
+app.post('/edit/:id', (req, res) => {
+    var hippo_id = req.params.id;
+    Hippo.findOne({_id: hippo_id})
+        .then(hippos => {
+            hippos.name = req.body.name;
+            hippos.description = req.body.description;
+            return hippos.save();
+        })
+        .then(hippos => {
+            res.redirect("/")
+        })
+        .catch(err => res.json(err));
+})
+
+app.get('/destroy/:id', (req, res) => {
+    var hippo_id = req.params.id;
+    Hippo.findOne({_id: hippo_id})
+        .then(hippos => {
+            hippos.remove({_id: hippo_id});
+            res.redirect('/')
+        })
+        .catch(err => res.json(err));
+})
 
 app.listen(8000, function () {
     console.log("server running on port 8000");
